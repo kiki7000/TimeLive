9685967f8f449e20fa829cf91995ef3ad9d0fbba
woohyunjng <woohyun.jng08@gmail.com>
2022-07-20 01:25:39 +0900

    feat(api/auth): naver login/callback request

--- TimeLive/api/auth.py
+++ TimeLive/api/auth.py
@@ -28,6 +28,20 @@ async def google_login(request: Request):
     })
 
 
+@Auth.get("/login/naver")
+async def naver_login(request: Request):
+    return json({
+        "login_url": create_url(
+            "https://nid.naver.com/oauth2.0/authorize?",
+            scope="email name",
+            state="",
+            redirect_uri=f"{request.app.config.BASE_URL}{request.app.url_for('auth.naver_callback')}",
+            response_type="code",
+            client_id=request.app.config.AUTH["NAVER_CLIENT_ID"]
+        )
+    })
+
+
 @Auth.get("/callback/google")
 async def google_callback(request: Request):
     async with ClientSession() as session:
@@ -43,6 +57,7 @@ async def google_callback(request: Request):
                 }
             ) as response:
                 data = await response.json()
+                print(data)
                 token = data["access_token"]
         except Exception as _:
             raise BadRequest
@@ -53,3 +68,30 @@ async def google_callback(request: Request):
         ) as response:
             data = await response.json()
             return json(data)
+
+
+@Auth.get("/callback/naver")
+async def naver_callback(request: Request):
+    async with ClientSession() as session:
+        try:
+            async with session.post(
+                "https://nid.naver.com/oauth2.0/token",
+                data={
+                    "client_id": request.app.config.AUTH["NAVER_CLIENT_ID"],
+                    "client_secret": request.app.config.AUTH["NAVER_CLIENT_SECRET"],
+                    "grant_type": "authorization_code",
+                    "code": request.args.get("code"),
+                    "state": ""
+                }
+            ) as response:
+                data = await response.json()
+                token = data["access_token"]
+        except Exception as _:
+            raise BadRequest
+
+        async with session.get(
+            "https://openapi.naver.com/v1/nid/me",
+            headers={"Authorization": f"Bearer {token}"}
+        ) as response:
+            data = await response.json()
+            return json(data["response"])
